2d61f66aa9ee16e7babd4db63bba4ad45864c475
Dmitry Kozhevnikov <dmitry.kozhevnikov@jetbrains.com>
2019-01-28 17:06:43 +0300

    Write measuring ninja

--- fake-modular-build/cdb-to-ninja.py
+++ fake-modular-build/cdb-to-ninja.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-
-import sys
+import argparse
 import json
 import shlex
 import os
@@ -36,10 +35,11 @@ def remove_input_and_output(args: List[str], input_file: str, wd: str) -> str:
 
 
 class CDBToNinjaBuilder:
-    def __init__(self):
-        self.compilers: Dict[str, str] = {} # dict of (exec name: var name)
+    def __init__(self, measuring_compilers_path: Optional[str]):
+        self.compilers: Dict[str, str] = {}  # dict of (exec name: var name)
         self.rules: Dict[str, Tuple[str, str]] = {}  # dict of (common_args: (rule_name, rule_text))
         self.edges: List[str] = []
+        self.measuring_compilers_path = measuring_compilers_path
 
     def add_cdb_command(self, command: str, input_file: str, wd: str):
         assert os.path.isabs(wd), 'Only absolute working dirs are supported!'
@@ -47,13 +47,16 @@ class CDBToNinjaBuilder:
         compiler_var_name = self.add_or_create_compiler(args[0])
         args[0] = '${}'.format(compiler_var_name)
         output_file = make_absolute(find_output(args), wd)
+        time_file = output_file + ".time.json" if self.measuring_compilers_path is not None else None
         input_file = make_absolute(input_file, wd)
         common_args = remove_input_and_output(args, input_file, wd)
-        self.add_build_edge(common_args, input_file, output_file, wd)
+        self.add_build_edge(common_args, input_file, output_file, time_file, wd)
 
-    def add_build_edge(self, common_args: str, input_file: str, output_file: str, working_dir: str):
+    def add_build_edge(self, common_args: str, input_file: str, output_file: str, implicit_output_file: Optional[str],
+                       working_dir: str):
         rule_name = self.find_or_add_rule(common_args, working_dir)
-        self.edges.append("build {}: {} {}".format(output_file, rule_name, input_file))
+        implicit_output_file_part = ' | {}'.format(implicit_output_file) if implicit_output_file else ''
+        self.edges.append("build {}{}: {} {}".format(output_file, implicit_output_file_part, rule_name, input_file))
 
     def find_or_add_rule(self, common_args: str, working_dir: str) -> str:
         key = common_args + working_dir
@@ -69,8 +72,9 @@ class CDBToNinjaBuilder:
         return rule_name
 
     def get_text(self):
+        compilers = self.get_target_compilers()
         return \
-            "\n".join(["{} = {}".format(name, exe) for exe, name in self.compilers.items()]) + \
+            "\n".join(["{} = {}".format(name, exe) for exe, name in compilers.items()]) + \
             "\n\n" + \
             "\n".join(rule_text for rule_name, rule_text in self.rules.values()) + \
             "\n\n" + \
@@ -84,22 +88,45 @@ class CDBToNinjaBuilder:
         self.compilers[compiler] = var_name
         return var_name
 
+    def get_target_compilers(self) -> Dict[str, str]:
+        if self.measuring_compilers_path is None:
+            return self.compilers
 
-def cdb_to_ninja(cdb) -> str:
-    builder = CDBToNinjaBuilder()
+        c_injected = False
+        cpp_injected = False
+        result = {}
+        for executable, var_name in self.compilers.items():
+            looks_like_cpp = '++' in executable
+            if looks_like_cpp and cpp_injected or not looks_like_cpp and c_injected:
+                raise RuntimeError("Can't inject compilers in {}".format(self.compilers))
+            result[os.path.join(self.measuring_compilers_path, 'clang++' if looks_like_cpp else 'clang')] = var_name
+        return result
+
+
+def cdb_to_ninja(cdb: Iterable[Mapping[str, str]], measuring_compiler_path: str) -> str:
+    builder = CDBToNinjaBuilder(measuring_compiler_path)
     for entry in cdb:
         builder.add_cdb_command(entry['command'], entry['file'], entry['directory'])
+
     return builder.get_text()
 
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: cdb-to-ninja <path-to-cdb> <path-to-output>")
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description='Prepare normal or measuring ninja build script out of CDB')
 
-    cdb = json.load(open(sys.argv[1]))
-    ninja = cdb_to_ninja(cdb)
-    open(sys.argv[2], 'w').write(ninja)
+    parser.add_argument('--cdb-path', help='path to CDB', required=True)
+    parser.add_argument('--output-path', help='path to ninja build script output', required=True)
+    parser.add_argument('--metadata-path', help='path to store metadata (i.e. .cpp/.o mappings)')
+    parser.add_argument('--measuring-compiler-path',
+                        help='path to measuring compilers (clang/clang++), omit to use original compiler')
+    args = parser.parse_args()
+
+    cdb = json.load(open(args.cdb_path))
+    ninja = cdb_to_ninja(cdb, args.measuring_compiler_path)
+    metadata = None
+    open(args.output_path, 'w').write(ninja)
+    if args.metadata_path:
+        open(args.metadata_path, 'w').write(metadata)
 
 
 if __name__ == '__main__':
